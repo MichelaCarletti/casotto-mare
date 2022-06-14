ed7d199554fc4c14d433b88c220fcceba3ccf9d5
Cla07 <94793707+Cla07@users.noreply.github.com>
2022-06-14 12:01:54 +0200

    aggiornamento codice

--- Casotto-mare-java/Casotto-mare/src/Ospite.java
+++ Casotto-mare-java/Casotto-mare/src/Ospite.java
@@ -58,17 +58,28 @@ public class Ospite extends Persona {
 	}
 
 	public void richiedi_preventivo() throws SQLException {
-		String url = "jdbc:mysql://127.0.0.1:3306/casottoDB"; // jdbc:msql://127.0.0.1:3306/casotto
-		Connection conn = DriverManager.getConnection(url, "Capriotti Riccardo", "");
+		String url = "jdbc:mysql://127.0.0.1:3306/casotto"; // jdbc:msql://127.0.0.1:3306/casotto
+		Connection conn = DriverManager.getConnection(url, "root", "claMysql");
 		Statement st = conn.createStatement();
 
 		// SELECT per ritornare posizione e costo dell'ombrellone
-		ResultSet rs = st.executeQuery("SELECT costo_ombrellone FROM Ombrellone");
+		ResultSet rs = st.executeQuery("SELECT  costo_ombrellone FROM Ombrellone");
 //		ResultSetMetaData rsmd = rs.getMetaData();
-		String costo = rs.getString("costo_ombrellone");
-//		se mettiamo il costo uguale la posizione diventa ininfluente
-//		String pos = rs.getString("posizione_ombrellone");
-		System.out.println("L'ombrellone costa " + costo);
+	
+	
+		//String costo = rs.getString("costo_ombrellone");
+		
+		ResultSetMetaData rsmd = rs.getMetaData();
+		int columnsNumber = rsmd.getColumnCount();
+
+		while (rs.next()) {
+			for (int i = 1; i <= columnsNumber; i++) {
+				System.out.print("Il costo ombrellone: "+rs.getString(i) + " "); 
+
+			}
+			System.out.println();
+		}
+		
 		conn.close();
 	}
 
@@ -119,124 +130,140 @@ public class Ospite extends Persona {
 		
 		
 		switch (p) {
-		case 1:
 		
-			ResultSet rs = st.executeQuery("SELECT id_ombrellone FROM prenotazione WHERE mezza_giornata = true AND giornata_intera = false AND giorni_consecutivi = false  ");
-			
+		case 1:
+
+			ResultSet rs = st.executeQuery(
+					"SELECT id_ombrellone FROM prenotazione WHERE mezza_giornata = true AND giornata_intera = false AND giorni_consecutivi = false  ");
+
 			ResultSetMetaData rsmd = rs.getMetaData();
 			int columnsNumber = rsmd.getColumnCount();
-			
-			
-			//System.out.println(st.getResultSet()); 
-			
+
+			// System.out.println(st.getResultSet());
+
 			while (rs.next()) {
 				for (int i = 1; i <= columnsNumber; i++) {
-					System.out.print("Ombrellone disponibile: "+rs.getString(i) + " "); // Stampa elementi di 1 riga
+					System.out.print("Ombrellone occupato: " + rs.getString(i) + " "); // Stampa elementi di 1 riga
 
 				}
 				System.out.println();
 			}
-			
-			//scelgo l'ombrellone 
-			System.out.println("Scegli l'ombrellone");
-			
-			int num = 0;
-			num = in.nextInt();
-			
-			
-	        //id_prenotazione giorno,id_ombrellone,username_cliente, mezza_giornata, giornata_intera, giorni_consecutivi
-
-			
 
 			System.out.println("Inserisci id_ombrellone: ");
-			
+
 			int id_ombrellone = in.nextInt();
 
-
-			int username_cliente = this.getId_persona();  //id_persona sarebbe l'username 
-
-		
-		
-
-			
+			int username_cliente = this.getId_persona(); // id_persona sarebbe l'username
 
 			System.out.println("Inserisco la prenotazione nel db");
 
-			String query = "insert into prenotazione (giorno,id_ombrellone,username_cliente, mezza_giornata, giornata_intera, giorni_consecutivi)" + "values (?, ?, ?, ?, ?, ? )";
+			String query = "insert into prenotazione (giorno,id_ombrellone,username_cliente, mezza_giornata, giornata_intera, giorni_consecutivi)"
+					+ "values (?, ?, ?, ?, ?, ? )";
 
 			PreparedStatement preparedStmt = conn.prepareStatement(query);
 
-			
-			
-			
 			// FUNZIONANTE
-		preparedStmt.setString(1, giorno);
-		preparedStmt.setInt(2, id_ombrellone);
-		preparedStmt.setInt(3, username_cliente);
-		preparedStmt.setBoolean(4, true);
-		preparedStmt.setBoolean(5, false);
-		preparedStmt.setBoolean(6, false);
-	
-		preparedStmt.execute();
-			
-			
+			preparedStmt.setString(1, giorno);
+			preparedStmt.setInt(2, id_ombrellone);
+			preparedStmt.setInt(3, username_cliente);
+			preparedStmt.setBoolean(4, true);
+			preparedStmt.setBoolean(5, false);
+			preparedStmt.setBoolean(6, false);
+
+			preparedStmt.execute();
+
 			break;
 		case 2:
-			rs = st.executeQuery("SELECT id_ombrellone FROM prenotazione WHERE mezza_giornata = false AND giornata_intera = true AND giorni_consecutivi = false");
-			
+			rs = st.executeQuery(
+					"SELECT id_ombrellone FROM prenotazione WHERE mezza_giornata = false AND giornata_intera = true AND giorni_consecutivi = false");
+
 			ResultSetMetaData resmd = rs.getMetaData();
 			int numberColomn = resmd.getColumnCount();
-			
-			
-			//System.out.println(st.getResultSet()); 
-			
+
+			// System.out.println(st.getResultSet());
+
 			while (rs.next()) {
 				for (int i = 1; i <= numberColomn; i++) {
-					System.out.print("Il tuo ombrellone è il numero: "+rs.getString(i) + " "); // Stampa elementi di 1 riga
+					System.out.print("Il tuo ombrellone è il numero: " + rs.getString(i) + " "); // Stampa elementi di 1
+																									// riga
 
 				}
 				System.out.println();
 			}
-			
-			
+
+			System.out.println("Inserisci id_ombrellone: ");
+
+			int idombrellone = in.nextInt();
+
+			int usernamecliente = this.getId_persona(); // id_persona sarebbe l'username
+
+			System.out.println("Inserisco la prenotazione nel db");
+
+			String q = "insert into prenotazione (giorno,id_ombrellone,username_cliente, mezza_giornata, giornata_intera, giorni_consecutivi)"
+					+ "values (?, ?, ?, ?, ?, ? )";
+
+			PreparedStatement preparedStmts = conn.prepareStatement(q);
+
+			// FUNZIONANTE
+			preparedStmts.setString(1, giorno);
+			preparedStmts.setInt(2, idombrellone);
+			preparedStmts.setInt(3, usernamecliente);
+			preparedStmts.setBoolean(4, false);
+			preparedStmts.setBoolean(5, true);
+			preparedStmts.setBoolean(6, false);
+
+			preparedStmts.execute();
+
 			break;
 		case 3:
 			System.out.println("Inserire il numero di giorni\n");
 			int d = in.nextInt();
-			rs = st.executeQuery("SELECT id_ombrellone FROM prenotazione WHERE mezza_giornata = false AND giornata_intera = false AND giorni_consecutivi = true");
-			
+			rs = st.executeQuery(
+					"SELECT id_ombrellone FROM prenotazione WHERE mezza_giornata = false AND giornata_intera = false AND giorni_consecutivi = true");
+
 			ResultSetMetaData resumd = rs.getMetaData();
 			int numbColomn = resumd.getColumnCount();
-			
-			
-			//System.out.println(st.getResultSet()); 
-			
+
+			// System.out.println(st.getResultSet());
+
 			while (rs.next()) {
 				for (int i = 1; i <= numbColomn; i++) {
-					System.out.print("Il tuo ombrellone è il numero: "+rs.getString(i) + " "); // Stampa elementi di 1 riga
+					System.out.print("Il tuo ombrellone è il numero: " + rs.getString(i) + " "); // Stampa elementi di 1
+																									// riga
 
 				}
 				System.out.println();
 			}
-			
-			
+
+			System.out.println("Inserisci id_ombrellone: ");
+
+			int Idombrellone = in.nextInt();
+
+			int Usernamecliente = this.getId_persona(); // id_persona sarebbe l'username
+
+			System.out.println("Inserisco la prenotazione nel db");
+
+			String quer = "insert into prenotazione (giorno,id_ombrellone,username_cliente, mezza_giornata, giornata_intera, giorni_consecutivi)"
+					+ "values (?, ?, ?, ?, ?, ? )";
+
+			PreparedStatement PreparedStmts = conn.prepareStatement(quer);
+
+			// FUNZIONANTE
+			PreparedStmts.setString(1, giorno);
+			PreparedStmts.setInt(2, Idombrellone);
+			PreparedStmts.setInt(3, Usernamecliente);
+			PreparedStmts.setBoolean(4, false);
+			PreparedStmts.setBoolean(5, false);
+			PreparedStmts.setBoolean(6, true);
+
+			PreparedStmts.execute();
+
 			break;
 		}
-		
-//		ResultSet res_set = st.executeQuery("SELECT * FROM prenotazione");
-
-	//	while (rs_set.isAfterLast()) {
-	//		System.out.println(res_set);
-//			lista_prenotazione.add(rs);
-		//}
 
 		conn.close();
+
 		
-		/**
-		 * query per prendere le prenotazioni legate all'ombrellone e confrontarle con
-		 * le prenotazioni effettuate foreach(tutte le prenotazioni effettuate){ if(data
-		 * == data_prenotazione){ stampa } }
-		 */
 		return false;
 
 		
@@ -258,7 +285,7 @@ public class Ospite extends Persona {
 	public boolean registrazione() throws SQLException {
 		// connessione db
 		String url = "jdbc:mysql://127.0.0.1:3306/casotto"; // jdbc:msql://127.0.0.1:3306/casotto
-		Connection conn = DriverManager.getConnection(url, "root", "mySql");
+		Connection conn = DriverManager.getConnection(url, "root", "claMysql");
 		Statement st = conn.createStatement();
 
 		Scanner in = new Scanner(System.in);
@@ -289,7 +316,7 @@ public class Ospite extends Persona {
 		}
 
 		// serve per costruire l'oggetto nel main richiamando il metodo registrazione e
-		// TODO inserire nel db
+
 		this.setNome(name);
 		this.setCognome(surname);
 		this.setEmail(e_mail);
@@ -318,9 +345,9 @@ public class Ospite extends Persona {
 
 	@Override
 	public boolean autenticazione() throws SQLException {
-		String url = "jdbc:mysql://127.0.0.1:3306/casottoDB"; // jdbc:msql://127.0.0.1:3306/casotto
+		String url = "jdbc:mysql://127.0.0.1:3306/casotto"; // jdbc:msql://127.0.0.1:3306/casotto
 		Connection conn = null;
-		conn = DriverManager.getConnection(url, "Capriotti Riccardo", "");
+		conn = DriverManager.getConnection(url, "root", "claMysql");
 		Statement st = conn.createStatement();
 
 		Scanner in = new Scanner(System.in);
@@ -360,10 +387,13 @@ public class Ospite extends Persona {
 	@Override
 	public void my_page() throws SQLException {
 		int number = 0;
-		System.out.println("1- Richiedi preventivo" + "\n" + "2- Prenota ombrellone" + "\n" + "3- Prenota tavolo");
-
+		System.out.println("1- Richiedi preventivo" + "\n" + "2- Prenota ombrellone" + "\n" + "3- Prenota tavolo" + "\n");
+		Scanner s = new Scanner(System.in);
+		
+		number =  s.nextInt();
 		switch (number) {
-
+	
+		
 		case 1:
 			richiedi_preventivo();
 			break;
